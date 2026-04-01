aa185275ed7137bf848739cb9ebbf509e271c0ef
Hyeuk Ryu <dynaryu@gmail.com>
2026-04-01 15:55:13 +1100

    add PIMS/python_code; gitignore

---
+++ PIMs/python_code/rips/kernels/__init__.py
@@ -0,0 +1,6 @@
+from rips.kernels.metropolis import *
+from rips.kernels.adaptive import *
+from rips.kernels.hit_and_run import *
+from rips.kernels.gaussian import *
+from rips.kernels.crank_nicolson import *
+from rips.kernels.modified_mh import *
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from adaptmcs-benchmarks!")
+
+
+if __name__ == "__main__":
+    main()
